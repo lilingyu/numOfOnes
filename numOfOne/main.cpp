b980c4d5443c1904570ff73854410ffbe3742ecc
Lingyu Li <lilingyu1991@foxmail.com>
2015-11-10 12:50:16 +0800

    Initial Commit

---
+++ numOfOne/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  numOfOne
+//
+//  Created by LiLingyu on 15/11/10.
+//  Copyright © 2015年 LiLingyu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
